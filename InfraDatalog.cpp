9ce18ab4139fcf3c24ecbd74fcf0578711678f63
U-ModerModemet\Ogelami <ogelami@gmail.com>
2015-05-21 20:07:56 +0200

    Exporting LEDGPIO

---
+++ InfraDatalog.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+//http://cholla.mmto.org/computers/beagle/hardware/pinout1-1024x585.png
+//https://github.com/derekmolloy/boneDeviceTree/blob/master/gpio/TestApplication.cpp
+
+// P9_12 = GPIO1_28 = (1x32) + 28 = 60
+#define LEDGPIO 60
+
+int main(int argc, char *argv[])
+{
+	gpio_export(LEDGPIO);
+	gpio_set_dir(ButtonGPIO, INPUT_PIN);
+	
+	return 0;
+}
