1d52b69f594a1f137b6dbe642920b90a6a0fcceb
Robin Dahlberg <Robin@KaffeFilter.local>
2015-06-07 21:09:09 +0200

    blah

--- InfraDatalog/InfraDatalog.cpp
+++ InfraDatalog/InfraDatalog.cpp
@@ -11,11 +11,11 @@ using namespace std;
 // P9_12 = GPIO1_28 = (1x32) + 28 = 60
 #define LEDGPIO 60
 
-void prettyOutput(unsigned short state, struct timeval timestamp)
+/*void prettyOutput(unsigned short state, struct timeval timestamp)
 {
 	printf("%d %d %d\n", state, timestamp.tv_sec, timestamp.tv_usec);
 //	fflush(stdout);
-}
+}*/
 
 int main(int argc, char *argv[])
 {
@@ -27,7 +27,7 @@ int main(int argc, char *argv[])
 	unsigned int previousState = 0;
 	unsigned int newState = 0;
 	
-	struct timeval timestamp;
+	struct timeval *timestamp = (struct timeval*)malloc(cycles * sizeof(timeval));
 
 	gpio_get_value(LEDGPIO, &previousState);
 	
@@ -39,8 +39,8 @@ int main(int argc, char *argv[])
 		}
 		while (previousState == newState);
 
-		gettimeofday(&timestamp, 0);
-		prettyOutput(newState, timestamp);
+		gettimeofday(&timestamp[i], 0);
+//		prettyOutput(newState, timestamp);
 		
 		previousState = newState;
 	}
